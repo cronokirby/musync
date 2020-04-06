8bf5fb6bbe9b93bfd992e9b052a1c44c9a3c4c9e
Lúcás Meier <cronokirby@gmail.com>
2020-04-06 19:04:54 +0200

    Add command line parsing using kong
    This adds an anonymous struct to allow us to parse command line
    arguments for our program. Right now we can parse things like:
    
      musync sync --root . musync.toml
      musync add musync.toml

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cronokirby/musync
 
 go 1.14
+
+require github.com/alecthomas/kong v0.2.4
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alecthomas/kong"
+)
+
+var cli struct {
+	Sync struct {
+		Root string `name:"root" help:"The directory to sync files to" type:"existingdir" default:"."`
+		Path string `arg:"" name:"path" help:"The file describing what to sync" type:"existingfile"`
+	} `cmd:"" help:"Sync music files"`
+	Add struct {
+		Path string `arg:"" name:"path" help:"The file containing the files to sync" type:"existingfile"`
+	} `cmd:"" help:"Add a new album or song to the syncing file"`
+}
+
+func sync(root string, path string) {
+	fmt.Printf("Sync, root: %s, path: %s\n", root, path)
+}
+
+func add(path string) {
+	fmt.Printf("Add, path: %s\n", path)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	ctx := kong.Parse(&cli)
+	switch ctx.Command() {
+	case "sync <path>":
+		sync(cli.Sync.Root, cli.Sync.Path)
+	case "add <path>":
+		add(cli.Add.Path)
+	}
 }
