9145faf6e975f2518933f05ceb03ce10dd6b48e6
Lúcás Meier <cronokirby@gmail.com>
2020-04-07 09:36:52 +0200

    Rename 'root' to 'out'

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 var cli struct {
 	Sync struct {
-		Root string `name:"root" help:"The directory to sync files to" type:"existingdir" default:"."`
+		Out  string `name:"out" help:"The directory to sync files to" type:"existingdir" default:"."`
 		Path string `arg:"" name:"path" help:"The file describing what to sync" type:"existingfile"`
 	} `cmd:"" help:"Sync music files"`
 	Add struct {
@@ -16,8 +16,8 @@ var cli struct {
 	} `cmd:"" help:"Add a new album or song to the syncing file"`
 }
 
-func sync(root string, path string) {
-	fmt.Printf("Sync, root: %s, path: %s\n", root, path)
+func sync(out string, path string) {
+	fmt.Printf("Sync, out: %s, path: %s\n", out, path)
 }
 
 func add(path string) {
@@ -28,7 +28,7 @@ func main() {
 	ctx := kong.Parse(&cli)
 	switch ctx.Command() {
 	case "sync <path>":
-		sync(cli.Sync.Root, cli.Sync.Path)
+		sync(cli.Sync.Out, cli.Sync.Path)
 	case "add <path>":
 		add(cli.Add.Path)
 	}
